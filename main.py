85f7c59f4ebf766b96e03596982abac5d72a2d46
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-12 12:18:21 +0300

    2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pygame
 import requests
 
-coords = ['49.106822', '55.795464']
+coords = ['37.620070', '55.753630']
 zoom = 10
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 
@@ -22,17 +22,31 @@ with open(map_file, "wb") as file:
 pygame.init()
 screen = pygame.display.set_mode((500, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
+scale = 5
+map_type = 'map'
 pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                if scale + 1 <= 19:
+                    scale += 1
+            if event.key == pygame.K_PAGEDOWN:
+                if scale - 1 >= 2:
+                    scale -= 1
+    zoom = str(scale)
+    map_api_params = {
+        "ll": ",".join(coords),
+        "z": zoom,
+        "l": map_type
+    }
     response = requests.get(map_api_server, params=map_api_params)
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
-
 os.remove(map_file)
